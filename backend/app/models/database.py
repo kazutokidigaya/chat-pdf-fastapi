4834e60ce25e79aa9630095d6b4d44d19866993e
karanT <2001karanpt@gmail.com>
2024-10-28 16:31:34 +0530

    update database.py for repetation

--- backend/app/models/database.py
+++ backend/app/models/database.py
@@ -14,10 +14,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set. Please check your .env file or environment variables.")
 
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
